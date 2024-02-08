a1b2e3e86cd786ba1509a29b184828ca9660ce33
Matias Silva <msil.igh@gmail.com>
2024-02-08 13:01:59 -0300

    arreglos 1.0

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ window.addEventListener('scroll', () => {
         navEl.classList.remove('navbar-scrolled')
     }
 })
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleDarkModeButton = document.getElementById('toggleDarkMode');
+    const body = document.body;
+
+    // Verifica si hay una preferencia almacenada en localStorage
+    const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+    // Aplica el modo oscuro si está habilitado
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+    }
+
+    // Cambia el modo oscuro al hacer clic en el botón
+    toggleDarkModeButton.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+        
+        // Guarda la preferencia en localStorage
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    });
+});
