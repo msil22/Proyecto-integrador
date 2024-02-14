4d5c3355442fa2dfe86c3f5a50c38dec02453cc5
Matias Silva <msil.igh@gmail.com>
2024-02-14 17:33:24 -0300

    as

--- script.js
+++ script.js
@@ -61,3 +61,16 @@ window.onclick = function (event) {
     modal.style.display = 'none';
   }
 };
+
+
+function toggleChat() {
+  var chatBox = document.getElementById('chatBox');
+  chatBox.style.display = chatBox.style.display === 'none' ? 'block' : 'none';
+}
+
+function sendMessage() {
+  var userInput = document.getElementById('userInput').value;
+  var chatMessages = document.getElementById('chatMessages');
+  chatMessages.innerHTML += '<p>Usuario: ' + userInput + '</p>';
+  document.getElementById('userInput').value = ''; // Limpiar el campo de entrada
+}
