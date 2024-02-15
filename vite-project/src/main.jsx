deedb7b2253250226db5e2e25b90a9325beb3adc
sofia-sil <155403667+sofia-sil@users.noreply.github.com>
2024-02-15 10:58:07 -0300

    Componentes

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import FormularioUser from './FormularioUser/FormularioUser.jsx'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    < FormularioUser />
   </React.StrictMode>,
 )
