3d2334354b5b16d86d48850ec6be8c98a18025a2
Daniel <daniel_m8c@hotmail.com>
2024-02-15 22:16:18 -0300

    Subiendo Moderar Empresas Perfil

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-/*import './App.css'*/
-=======
 import React from 'react'
 import Footer from './components/Footer/Footer.jsx'
 import Navbar from './components/navbars/NavbarMain.jsx'
 
->>>>>>> 5baba39cae73871520777c4f218f234f0423c62c
 
 function App() {
 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import Footer from'./Footer/Footer.jsx'
-import Navbar from './navbars/Navbar.jsx'
-import Moderar_Empresas_Perfil from './Moderar_Empresas_Perfil/Moderar_Empresas_Perfil.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Navbar/>
-    <Moderar_Empresas_Perfil/>
-    
- 
-    <Footer/>
-=======
 import Footer from './components/Footer/Footer.jsx'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import App from './App.jsx'
 import AutorizacionPerfiles from './components/AutorizacionPerfiles/AutorizacionPerfiles.jsx'
+import Moderar_Empresas_Perfil from './components/Moderar_Empresas_Perfil/Moderar_Empresas_Perfil.jsx'
 
 
 
@@ -32,12 +20,15 @@ const router = createBrowserRouter([
   {
     path: "autorizacionPerfiles",
     element: <AutorizacionPerfiles />
+  },
+  {
+    path: "moderar_empresas_perfil",
+    element: <Moderar_Empresas_Perfil />
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
->>>>>>> 5baba39cae73871520777c4f218f234f0423c62c
   </React.StrictMode>,
 )
